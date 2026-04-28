7ed7faffedfa3304305f63aa0403c1998af12386
wizgowaim <wizgow.business@gmail.com>
2026-04-28 04:16:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
+
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const fs = require("fs");
+const { startAutoLeaderboard } = require("./services/autoLeaderboard");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
 client.commands = new Collection();
 
 const commandFiles = fs.readdirSync("./commands").filter(f => f.endsWith(".js"));
@@ -18,9 +23,11 @@ client.once("ready", () => {
   });
 
   console.log(`✅ Connecté en tant que ${client.user.tag} en mode Ne pas déranger`);
+
+  startAutoLeaderboard(client);
 });
 
-client.on("interactionCreate", async (interaction) => {
+client.on("interactionCreate", async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
   const command = client.commands.get(interaction.commandName);
